ce8b054976fd2458d6e1b23b90cb2d2d1ac293ab
ainebyoonabridget <bridgetaainebyoona@gmail.com>
2020-07-27 17:52:01 +0300

    birthday

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Touch the cake 🎂 and enjoy the party 🎶🎵🎉🎊");
+
+window.addEventListener('click', function () {
+    
+    var audio = document.getElementById("HBD");
+    audio.play();
+    
+});
